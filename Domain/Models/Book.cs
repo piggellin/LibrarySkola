deebbc1ce7d2e9fa838d49d98afc823a12ff5fff
piggellin <elinpettersson15@gmail.com>
2024-11-21 15:58:49 +0100

    removed unused "using" added bookcontroller and tried to create the api

--- Domain/Models/Book.cs
+++ Domain/Models/Book.cs
@@ -3,8 +3,16 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }   
-        public Author Author { get; set; }
+        public string? Title { get; set; }   
         public int AuthorId { get; set; }
+
+        public Book() { } 
+
+        public Book(int id, string title, int authorId)
+        {
+            Id = id;
+            Title = title;
+            AuthorId = authorId;
+        }
     }
 }
---
+++ Presentation/Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using Application.Commands;
+using Application.Handlers;
+using Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookHandler _handler;
+
+        public BookController(BookHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _handler.Handle(new GetBookByIdQuery { Id = id });
+            return book != null ? Ok(book) : NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook([FromBody] CreateBookCommand command)
+        {
+            _handler.Handle(command);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBook([FromBody] UpdateBookCommand command)
+        {
+            _handler.Handle(command);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            _handler.Handle(new DeleteBookCommand { Id = id });
+            return Ok();
+        }
+    }
+
+}
